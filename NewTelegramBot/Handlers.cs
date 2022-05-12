fc365ead38afe4b0932ad756f60066b9b4d25aa2
Kirya <kirya,krotov.03@mail.ru>
2022-05-12 16:06:57 +1000

    Добавьте файлы проекта.

---
+++ NewTelegramBot/Handlers.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Telegram.Bot.Examples.Polling
+{
+
+    public class Handlers
+    {
+        public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var handler = update.Type switch
+            {
+                // UpdateType.Unknown:
+                // UpdateType.ChannelPost:
+                // UpdateType.EditedChannelPost:
+                // UpdateType.ShippingQuery:
+                // UpdateType.PreCheckoutQuery:
+                // UpdateType.Poll:
+                UpdateType.Message => BotOnMessageReceived(botClient, update.Message!),
+                UpdateType.EditedMessage => BotOnMessageReceived(botClient, update.EditedMessage!),
+                UpdateType.CallbackQuery => BotOnCallbackQueryReceived(botClient, update.CallbackQuery!),
+                UpdateType.InlineQuery => BotOnInlineQueryReceived(botClient, update.InlineQuery!),
+                UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(botClient, update.ChosenInlineResult!),
+                _ => UnknownUpdateHandlerAsync(botClient, update)
+            };
+
+            try
+            {
+                await handler;
+            }
+            catch (Exception exception)
+            {
+                await HandleErrorAsync(botClient, exception, cancellationToken);
+            }
+        }
+
+        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
+        {
+            List<string> listUsers = new List<string>();
+            #region
+            listUsers.Add("someuser");
+            #endregion
+
+            List<string> listSpam = new List<string>();
+            #region
+            listSpam.Add("123");
+            listSpam.Add("1 2 3");
+            listSpam.Add("http");
+            listSpam.Add("https");
+            listSpam.Add("www");
+            listSpam.Add("man");
+            listSpam.Add("vostretsova");
+            listSpam.Add("вострецова");
+            listSpam.Add("dfgdg");
+            #endregion
+            Console.WriteLine($"Получено новое сообщение, его тип: {message.Type}");
+            if (message.Type != MessageType.Text)
+                return;
+
+            bool yesSpam = listSpam.Any(ls => message.Text.ToLower().Contains(ls));
+            bool yesUser = listUsers.Any(lu => message.From.ToString().Contains(lu));
+
+            if (yesSpam | yesUser)
+            {
+                await botClient.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                Console.WriteLine($"Удалено спам - сообщение типа {message.Type}");
+            }
+        }
+
+        // Process Inline Keyboard callback data
+        private static async Task BotOnCallbackQueryReceived(ITelegramBotClient botClient, CallbackQuery callbackQuery)
+        {
+            await botClient.AnswerCallbackQueryAsync(
+                callbackQueryId: callbackQuery.Id,
+                text: $"Received {callbackQuery.Data}");
+
+            await botClient.SendTextMessageAsync(
+                chatId: callbackQuery.Message.Chat.Id,
+                text: $"Received {callbackQuery.Data}");
+        }
+
+        private static async Task BotOnInlineQueryReceived(ITelegramBotClient botClient, InlineQuery inlineQuery)
+        {
+            Console.WriteLine($"Received inline query from: {inlineQuery.From.Id}");
+
+            InlineQueryResult[] results = {
+            // displayed result
+            new InlineQueryResultArticle(
+                id: "3",
+                title: "TgBots",
+                inputMessageContent: new InputTextMessageContent(
+                    "hello"
+                )
+            )
+        };
+
+            await botClient.AnswerInlineQueryAsync(inlineQueryId: inlineQuery.Id,
+                                                   results: results,
+                                                   isPersonal: true,
+                                                   cacheTime: 0);
+        }
+
+        private static Task BotOnChosenInlineResultReceived(ITelegramBotClient botClient, ChosenInlineResult chosenInlineResult)
+        {
+            Console.WriteLine($"Received inline result: {chosenInlineResult.ResultId}");
+            return Task.CompletedTask;
+        }
+
+        private static Task UnknownUpdateHandlerAsync(ITelegramBotClient botClient, Update update)
+        {
+            Console.WriteLine($"Unknown update type: {update.Type}");
+            return Task.CompletedTask;
+        }
+    }
+} 
---
+++ NewTelegramBot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using System.Threading;
+using Telegram.Bot.Examples.Polling;
+using Telegram.Bot.Extensions.Polling;
+
+namespace NewTelegramBot
+{
+    class Program
+    {
+        private static string token { get; set; } = "5363694948:AAF6fkkHl6xvcag1DVCZdrgEpsdN0TgW1Zw";
+        private static TelegramBotClient Bot;
+        static void Main(string[] args)
+        {
+            Bot = new TelegramBotClient(token);
+
+            using var cts = new CancellationTokenSource();
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            ReceiverOptions receiverOptions = new() { AllowedUpdates = { } };
+            Bot.StartReceiving(Handlers.HandleUpdateAsync,
+                               Handlers.HandleErrorAsync,
+                               receiverOptions,
+                               cts.Token);
+
+            Console.WriteLine($"" +
+                $"Бот запущен и ждет сообщения...");
+            Console.ReadLine();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
+        }
+    }
+}
+
+//5363694948:AAF6fkkHl6xvcag1DVCZdrgEpsdN0TgW1Zw    Telegram_Bot
